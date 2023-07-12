f82cfafffe70d37e3d13f29f952fd083cb03668c
VelinKarchev <velinkarchev@gmail.com>
2023-07-12 10:59:35 +0300

    rhombus

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        By given dimensions print the pattern consisting of many embedded rhombs represented with
+        “/” and “\” and separated by space and “.” for the rest of the pattern. The pattern looks like:
+    Input
+    The input data should be read from the console.
+    You have an integer number N (always even number) showing the width and the height of the most outer rhomb.
+    The width and the height will always be equal number of symbols on the terminal.
+    The input data will always be valid and in the format described. There is no need to check it explicitly.
+    Output
+    The output should be printed on the console.
+    Use the “/” and “\” characters to print the rhomb sides and “.” (Dot) for the rest.
+    You should print exactly one space between each rhomb. See the examples below.
+    Constraints
+    N will always be a positive even number between 6 and 80 inclusive.
+
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        int outerRhombSize = Integer.parseInt(scanner.nextLine());
+
+
+
+    }
+}
---
+++ src/Rhomb.java
@@ -0,0 +1,16 @@
+public class Rhomb {
+    private int rhombSize;
+
+    public Rhomb(int rhombSize) {
+        this.rhombSize = rhombSize;
+    }
+
+    public int getRhombSize() {
+        return rhombSize;
+    }
+
+    public void setRhombSize(int rhombSize) {
+        this.rhombSize = rhombSize;
+    }
+
+}
